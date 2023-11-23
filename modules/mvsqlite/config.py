b9cc867ae31a5420fb382609caf154697e761f90
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-23 05:17:36 -0800

    Add check for Cargo before building mvsqlite

--- modules/mvsqlite/config.py
+++ modules/mvsqlite/config.py
@@ -1,7 +1,15 @@
 # config.py
 
+import subprocess
 
 def can_build(env, platform):
+    try:
+        subprocess.check_output(["cargo", "-V"], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        print("Cargo not found. mvsqlite build skipped.")
+        if platform == "windows":
+            print("Use `scoop install rustup-gnu` to install.")
+        return False
     if platform == "ios" or platform == "web" or platform == "android":
         return False
     return True
