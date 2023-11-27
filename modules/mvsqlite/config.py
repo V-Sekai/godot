34c995cf57cdbb3012eb28b54a0c52bd90620ed4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-26 20:06:33 -0800

    Update mvsqlite build configuration for Windows

--- modules/mvsqlite/config.py
+++ modules/mvsqlite/config.py
@@ -9,8 +9,6 @@ def can_build(env, platform):
         return False
     if platform == "windows" and env["use_mingw"] and env["use_llvm"]:
         return False
-    if platform == "windows" and not env["use_mingw"]:
-        return False
     try:
         subprocess.check_output(["rustup", "--version"], stderr=subprocess.STDOUT)
         subprocess.check_output(["cargo", "--version"], stderr=subprocess.STDOUT)
