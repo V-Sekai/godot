47ca7bc2e95e1175e2156ef5dffcdb17dfa8d5c2
K. S. Ernest (iFire) Lee~ <ernest.lee@chibifire.com>
2023-12-01 15:51:00 -0800

    git subrepo pull --force modules/many_bone_ik
    subrepo:
      subdir:   "modules/many_bone_ik"
      merged:   "51684cf346"
    upstream:
      origin:   "https://github.com/V-Sekai/many_bone_ik.git"
      branch:   "main"
      commit:   "51684cf346"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

--- modules/many_bone_ik/src/many_bone_ik_3d.h
+++ modules/many_bone_ik/src/many_bone_ik_3d.h
@@ -77,7 +77,6 @@ class ManyBoneIK3D : public Node3D {
 	NodePath skeleton_node_path = NodePath("..");
 	int32_t ui_selected_bone = -1, stabilize_passes = 4;
 	bool is_gizmo_dirty = false;
-	Ref<SceneTreeTimer> timer = memnew(SceneTreeTimer);
 	void _on_timer_timeout();
 	void update_ik_bones_transform();
 	void update_skeleton_bones_transform();
@@ -161,6 +160,8 @@ public:
 	void set_kusudama_limit_cone_count(int32_t p_constraint_index, int32_t p_count);
 	void set_kusudama_limit_cone_center(int32_t p_constraint_index, int32_t p_index, Vector3 p_center);
 	void set_kusudama_limit_cone_radius(int32_t p_constraint_index, int32_t p_index, float p_radius);
+	real_t get_kusudama_twist_current(int32_t p_index) const;
+	void set_kusudama_twist_current(int32_t p_index, real_t p_rotation);
 	ManyBoneIK3D();
 	~ManyBoneIK3D();
 	void set_dirty();
