2f8a317cec141e3ce22bad6a812256805948d2fa
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-03-12 03:46:02 -0700

    Refactor material loading process.

--- modules/mtlx/godot/material_x_3d.h
+++ modules/mtlx/godot/material_x_3d.h
@@ -1,3 +1,33 @@
+/**************************************************************************/
+/*  material_x_3d.h                                                       */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
 #ifndef MATERIAL_X_H
 #define MATERIAL_X_H
 
@@ -6,9 +36,9 @@
 #include "core/io/resource_importer.h"
 #include "scene/resources/material.h"
 
-// #include <MaterialXRenderGlsl/GLTextureHandler.h>
-// #include <MaterialXRenderGlsl/GLUtil.h>
-// #include <MaterialXRenderGlsl/TextureBaker.h>
+#include <MaterialXRenderGlsl/GLTextureHandler.h>
+#include <MaterialXRenderGlsl/GLUtil.h>
+#include <MaterialXRenderGlsl/TextureBaker.h>
 
 #include <MaterialXRender/Harmonics.h>
 #include <MaterialXRender/OiioImageLoader.h>
@@ -23,11 +53,13 @@
 
 #include <MaterialXCore/Util.h>
 
-// #include <MaterialXGenGlsl/GlslShaderGenerator.h>
-// #include <MaterialXGenShader/UnitSystem.h>
-// #include <MaterialXRenderGlsl/GlslProgram.h>
+#include <MaterialXGenGlsl/GlslShaderGenerator.h>
+#include <MaterialXGenShader/UnitSystem.h>
+#include <MaterialXRenderGlsl/GlslProgram.h>
 
 #include <MaterialXGenGlsl/EsslShaderGenerator.h>
+
+// TODO: FIRE 2024-03-12 - Cleanup includes.
 
 #include <iostream>
 
@@ -36,7 +68,7 @@ namespace mx = MaterialX;
 class MTLXLoader : public Resource {
 	GDCLASS(MTLXLoader, Resource);
 	mx::DocumentPtr _stdLib;
-	// mx::GlslRendererPtr _renderer;
+
 protected:
 	static void _bind_methods() {
 		ClassDB::bind_method(D_METHOD("_load", "path", "original_path", "use_sub_threads", "cache_mode"), &MTLXLoader::_load);
