9ff0a9a21de6fd23f5058ab15247912bf48c0763
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-05-02 21:04:18 -0700

    LibGodot Feature
    * Add a new GodotInstance GDCLASS that provides startup, shutdown and iteration commands
    * Add a new GDExtension entry point that creates a new Godot instance and returns a GodotInstance object.
    * This object can be used to control the GodotInstance.
    * Allow specifying an external native rendering surface handle to render Godot into the UI of a host application.
      * It is also possible to embed multiple Godot windows into the UI of a host application.
      * Currently supported on MacOS and iOS
    
    Sample Apps: https://github.com/migeran/libgodot_project
    
    Developed by [Migeran](https://migeran.com)
    
    Sponsors & Acknowledgements:
    
    * Initial development sponsored by [Smirk Software](https://www.smirk.gg/)
    * Rebasing to Godot 4.3 and further development sponsored by [Xibbon Inc.](https://xibbon.com)
    * The GDExtension registration of the host process & build system changes were based
      on @Faolan-Rad's LibGodot PR: https://github.com/godotengine/godot/pull/72883
    
    Co-Authored-By: Gabor Koncz <gabor.koncz@migeran.com>
    
    Run pre-commit run -a
    
    Fix error handling in window creation functions
    
    Memdelete on a RefCounted object is not valid. Use unref.
    
    Update Vulkan surface creation to use a method for getting the window handle.
    
    Add native surface rendering support for iOS.
    
    Add Vulkan rendering support in core driver types
    
    Add rendering_native_surface_android include
    
    Add rendering_native_surface_windows include
    
    Update iOS rendering layer creation method to use a different cast
    
    Run code formatting.
    
    Switch memdelete to unref for RenderingNativeSurfaceWindows
    
    Add include for rendering native surface on Windows
    
    Update Vulkan and D3D12 rendering code for Windows
    
    Refactor memory allocation functions to avoid compiler error.
    
    Apply class reference.
    
    Add support for D3D12 rendering context driver creation.
    
    Update platform detection logic and handle selected platform based on SCons version
    
    Make class registration for Wayland and X11 surfaces as internal classes.
    
    Keep define for MemoryBarrier in rendering_device_driver_d3d12.h.
    
    Change memory barrier struct names to MemoryAccessBarrier

--- main/main.cpp
+++ main/main.cpp
@@ -733,6 +733,7 @@ Error Main::test_setup() {
 
 	ClassDB::set_current_api(ClassDB::API_CORE);
 #endif
+	register_core_platform_apis();
 	register_platform_apis();
 
 	// Theme needs modules to be initialized so that sub-resources can be loaded.
@@ -794,6 +795,7 @@ void Main::test_cleanup() {
 	uninitialize_modules(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 	unregister_platform_apis();
+	unregister_core_platform_apis();
 	unregister_driver_types();
 	unregister_scene_types();
 
@@ -804,6 +806,7 @@ void Main::test_cleanup() {
 	GDExtensionManager::get_singleton()->deinitialize_extensions(GDExtension::INITIALIZATION_LEVEL_SERVERS);
 	uninitialize_modules(MODULE_INITIALIZATION_LEVEL_SERVERS);
 	unregister_server_types();
+	unregister_core_server_types();
 
 	EngineDebugger::deinitialize();
 	OS::get_singleton()->finalize();
@@ -889,7 +892,7 @@ int Main::test_entrypoint(int argc, char *argv[], bool &tests_need_run) {
  *   in help, it's a bit messy and should be globalized with the setup() parsing somehow.
  */
 
-Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_phase) {
+Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_phase, GDExtensionInitializationFunction p_init_func) {
 	Thread::make_main_thread();
 	set_current_thread_safe_for_nodes(true);
 
@@ -907,6 +910,8 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 
 	register_core_types();
 	register_core_driver_types();
+
+	register_core_platform_apis();
 
 	MAIN_PRINT("Main: Initialize Globals");
 
@@ -1789,7 +1794,7 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 	OS::get_singleton()->ensure_user_data_dir();
 
 	initialize_modules(MODULE_INITIALIZATION_LEVEL_CORE);
-	register_core_extensions(); // core extensions must be registered after globals setup and before display
+	register_core_extensions(p_init_func); // core extensions must be registered after globals setup and before display
 
 	ResourceUID::get_singleton()->load_from_cache(true); // load UUIDs from cache.
 
@@ -2420,6 +2425,9 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 
 	message_queue = memnew(MessageQueue);
 
+	// Register Core Server Types
+	register_core_server_types();
+
 	Thread::release_main_thread(); // If setup2() is called from another thread, that one will become main thread, so preventively release this one.
 	set_current_thread_safe_for_nodes(false);
 
@@ -2469,6 +2477,7 @@ error:
 		memdelete(packed_data);
 	}
 
+	unregister_core_platform_apis();
 	unregister_core_driver_types();
 	unregister_core_extensions();
 	unregister_core_types();
@@ -4232,6 +4241,7 @@ void Main::cleanup(bool p_force) {
 	GDExtensionManager::get_singleton()->deinitialize_extensions(GDExtension::INITIALIZATION_LEVEL_SERVERS);
 	uninitialize_modules(MODULE_INITIALIZATION_LEVEL_SERVERS);
 	unregister_server_types();
+	unregister_core_server_types();
 
 	EngineDebugger::deinitialize();
 
