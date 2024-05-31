014d7c918d38064bdc562d28868d81b461a8b3ec
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-05-31 13:06:06 -0700

    Apply style diff.

--- modules/robust_skin_weight_transfer/src/cli.py
+++ modules/robust_skin_weight_transfer/src/cli.py
@@ -1,12 +1,14 @@
 import argparse
-import os
+
 
 def parse_arguments():
-    argument_parser = argparse.ArgumentParser(description='Process some meshes.')
-    argument_parser.add_argument('--source_mesh', type=str, default='../meshes/sphere.obj',
-                        help='Path to the source mesh file')
-    argument_parser.add_argument('--target_mesh', type=str, default='../meshes/grid.obj',
-                        help='Path to the target mesh file')
+    argument_parser = argparse.ArgumentParser(description="Process some meshes.")
+    argument_parser.add_argument(
+        "--source_mesh", type=str, default="../meshes/sphere.obj", help="Path to the source mesh file"
+    )
+    argument_parser.add_argument(
+        "--target_mesh", type=str, default="../meshes/grid.obj", help="Path to the target mesh file"
+    )
 
     arguments = argument_parser.parse_args()
     return arguments
