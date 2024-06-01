7f736ca1f1797540ae8c519a9be8496e6337199c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-05-31 18:18:05 -0700

    Add some typing.

--- modules/robust_skin_weight_transfer/src/cli.py
+++ modules/robust_skin_weight_transfer/src/cli.py
@@ -1,8 +1,9 @@
 import argparse
+from argparse import ArgumentParser, Namespace
 
 
-def parse_arguments():
-    argument_parser = argparse.ArgumentParser(description="Process some meshes.")
+def parse_arguments() -> Namespace:
+    argument_parser: ArgumentParser = argparse.ArgumentParser(description="Process some meshes.")
     argument_parser.add_argument(
         "--source_mesh", type=str, default="../meshes/sphere.obj", help="Path to the source mesh file"
     )
@@ -10,5 +11,5 @@ def parse_arguments():
         "--target_mesh", type=str, default="../meshes/grid.obj", help="Path to the target mesh file"
     )
 
-    arguments = argument_parser.parse_args()
+    arguments: Namespace = argument_parser.parse_args()
     return arguments
