38ac35694115ff02b9adfd2ea791837df24004fc
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-07-01 07:08:55 -0700

    Add linux support.

--- modules/riscv/config.py
+++ modules/riscv/config.py
@@ -1,5 +1,5 @@
 def can_build(env, platform):
-    return platform == "windows" or platform == "macos"
+    return platform == "windows" or platform == "macos" or platform == "linux"
 
 
 def configure(env):
