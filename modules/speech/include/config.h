7eed056dc3cd48a1ffc24596b9f008b0f8747c98
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-08-18 14:36:49 -0700

    Disable sse[2,3,4.1] support

--- modules/speech/include/config.h
+++ modules/speech/include/config.h
@@ -173,10 +173,10 @@
 /* #define OPUS_X86_MAY_HAVE_AVX 1 */
 
 /* Compiler supports X86 SSE Intrinsics */
-#define OPUS_X86_MAY_HAVE_SSE 1
+// #define OPUS_X86_MAY_HAVE_SSE 1
 
 /* Compiler supports X86 SSE2 Intrinsics */
-#define OPUS_X86_MAY_HAVE_SSE2 1
+// #define OPUS_X86_MAY_HAVE_SSE2 1
 
 /* Compiler supports X86 SSE4.1 Intrinsics */
 /* #define OPUS_X86_MAY_HAVE_SSE4_1 1 */
@@ -185,10 +185,10 @@
 /* #undef OPUS_X86_PRESUME_AVX */
 
 /* Define if binary requires SSE intrinsics support */
-#define OPUS_X86_PRESUME_SSE 1
+// #define OPUS_X86_PRESUME_SSE 1
 
 /* Define if binary requires SSE2 intrinsics support */
-#define OPUS_X86_PRESUME_SSE2 1
+// #define OPUS_X86_PRESUME_SSE2 1
 
 /* Define if binary requires SSE4.1 intrinsics support */
 /* #undef OPUS_X86_PRESUME_SSE4_1 */
