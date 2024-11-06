65ec04b214c71f6ae762e20efea992da7cb9bb50
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-11-05 19:41:37 -0800

    git subrepo clone https://github.com/V-Sekai/godot-cmark.git modules/cmark
    subrepo:
      subdir:   "modules/cmark"
      merged:   "282962c5e0"
    upstream:
      origin:   "https://github.com/V-Sekai/godot-cmark.git"
      branch:   "main"
      commit:   "282962c5e0"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "73a0129"

---
+++ modules/cmark/resource_importer_commonmark.cpp
@@ -0,0 +1,103 @@
+/*************************************************************************/
+/*  resource_importer_json.cpp                                           */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#include "resource_importer_commonmark.h"
+#include "core/error/error_macros.h"
+#include "core/io/file_access_pack.h"
+
+#include "cmark-gfm.h"
+#include "core/os/memory.h"
+#include <stdint.h>
+
+String ResourceImporterCommonmark::get_preset_name(int p_idx) const {
+	return String();
+}
+
+void ResourceImporterCommonmark::get_import_options(List<ImportOption> *r_options, int p_preset) const {
+}
+
+bool ResourceImporterCommonmark::get_option_visibility(const String &p_option, const Map<StringName, Variant> &p_options) const {
+	return true;
+}
+
+String ResourceImporterCommonmark::get_importer_name() const {
+	return "Commonmark";
+}
+
+String ResourceImporterCommonmark::get_visible_name() const {
+	return "Commonmark";
+}
+
+void ResourceImporterCommonmark::get_recognized_extensions(List<String> *p_extensions) const {
+	p_extensions->push_back("md");
+}
+
+String ResourceImporterCommonmark::get_save_extension() const {
+	return "res";
+}
+
+String ResourceImporterCommonmark::get_resource_type() const {
+	return "CommonmarkData";
+}
+
+int ResourceImporterCommonmark::get_preset_count() const {
+	return 0;
+}
+
+Error ResourceImporterCommonmark::import(const String &p_source_file, const String &p_save_path, const Map<StringName, Variant> &p_options, List<String> *r_platform_variants, List<String> *r_gen_files, Variant *r_metadata) {
+
+	FileAccess *file = FileAccess::create(FileAccess::ACCESS_RESOURCES);
+	Error err;
+	String commonmark = file->get_file_as_string(p_source_file, &err);
+	ERR_FAIL_COND_V(err != OK, FAILED);
+	Ref<CommonmarkData> commonmark_data;
+	commonmark_data.instantiate();
+	commonmark_data->set_html(commonmark_data->convert_commonmark(commonmark));
+	return ResourceSaver::save(p_save_path + ".res", commonmark_data);
+}
+
+String CommonmarkData::get_html() const {
+	return html;
+}
+
+void CommonmarkData::set_html(const String p_string) {
+	html = p_string;
+}
+
+String CommonmarkData::convert_commonmark(String p_string) {
+	Vector<uint8_t> string_bytes = p_string.to_utf8_buffer();
+	char *cmark_bytes = cmark_markdown_to_html((const char *)string_bytes.ptr(), string_bytes.size() - 1, CMARK_OPT_DEFAULT);
+	String new_string;
+	new_string.parse_utf8(cmark_bytes);
+	if (cmark_bytes) {
+		free(cmark_bytes);
+	}
+	return new_string;
+}
---
+++ modules/cmark/resource_importer_commonmark.h
@@ -0,0 +1,88 @@
+/*************************************************************************/
+/*  resource_importer_json.h  	                                         */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+#ifndef RESOURCE_IMPORTER_JSON
+#define RESOURCE_IMPORTER_JSON
+
+#include "core/io/json.h"
+#include "core/io/resource_importer.h"
+#include "core/io/resource_saver.h"
+#include "core/object/class_db.h"
+#include "core/object/object.h"
+
+class CommonmarkData : public Resource {
+	GDCLASS(CommonmarkData, Resource);
+	String html;
+
+protected:
+	static void _bind_methods() {
+
+		ClassDB::bind_method(D_METHOD("set_html", "html"), &CommonmarkData::set_html);
+		ClassDB::bind_method(D_METHOD("get_html"), &CommonmarkData::get_html);
+		ClassDB::bind_method(D_METHOD("convert_commonmark"), &CommonmarkData::convert_commonmark);
+
+		ADD_PROPERTY(PropertyInfo(Variant::STRING, "html"), "set_html", "get_html");
+	}
+
+public:
+	String get_html() const;
+	void set_html(String p_string);
+	String convert_commonmark(const String p_string);
+	CommonmarkData() {}
+	~CommonmarkData() {}
+};
+
+class ResourceImporterCommonmark : public ResourceImporter {
+	GDCLASS(ResourceImporterCommonmark, ResourceImporter);
+
+public:
+	virtual String get_importer_name() const override;
+	virtual String get_visible_name() const override;
+	virtual void get_recognized_extensions(List<String> *p_extensions) const override;
+	virtual String get_save_extension() const override;
+	virtual String get_resource_type() const override;
+
+	virtual int get_preset_count() const override;
+	virtual String get_preset_name(int p_idx) const override;
+
+	virtual void get_import_options(List<ImportOption> *r_options,
+			int p_preset = 0) const override;
+	virtual bool
+	get_option_visibility(const String &p_option,
+			const Map<StringName, Variant> &p_options) const override;
+	virtual Error import(const String &p_source_file, const String &p_save_path,
+			const Map<StringName, Variant> &p_options,
+			List<String> *r_platform_variants,
+			List<String> *r_gen_files = NULL,
+			Variant *r_metadata = NULL) override;
+
+	ResourceImporterCommonmark() {}
+	~ResourceImporterCommonmark() {}
+};
+#endif // RESOURCE_IMPORTER_LOTTIE
