fe547266ba0ec17f1ef72dc68477f32f45b47fd4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-11-09 11:48:29 -0800

    Add minimum Cost Flows

--- modules/bimdf/bimdf.cpp
+++ modules/bimdf/bimdf.cpp
@@ -75,3 +75,60 @@ void BIMDF::solve() {
 	std::string output = buffer.str();
 	print_line(output.c_str());
 }
+
+// https://developers.google.com/optimization/flow/mincostflow
+
+// Minimum Cost Flows
+
+// Closely related to the max flow problem is the minimum cost (min cost) flow problem, in which each arc in the graph has a unit cost for transporting material across it. The problem is to find a flow with the least total cost.
+
+// The min cost flow problem also has special nodes, called supply nodes or demand nodes, which are similar to the source and sink in the max flow problem. Material is transported from supply nodes to demand nodes.
+
+//     At a supply node, a positive amount — the supply — is added to the flow. A supply could represent production at that node, for example.
+//     At a demand node, a negative amount — the demand — is taken away from the flow. A demand could represent consumption at that node, for example.
+
+// For convenience, we'll assume that all nodes, other than supply or demand nodes, have zero supply (and demand).
+
+// For the min cost flow problem, we have the following flow conservation rule, which takes the supplies and demands into account:
+// Note: At each node, the total flow leading out of the node minus the total flow leading in to the node equals the supply (or demand) at that node.
+
+// The graph below shows a min cost flow problem. The arcs are labeled with pairs of numbers: the first number is the capacity and the second number is the cost. The numbers in parentheses next to the nodes represent supplies or demands. Node 0 is a supply node with supply 20, while nodes 3 and 4 are demand nodes, with demands -5 and -15, respectively.
+
+// network cost flow graph
+// Import the libraries
+
+// import numpy as np
+
+// from ortools.graph.python import min_cost_flow
+
+// Declare the solver
+
+// # Instantiate a SimpleMinCostFlow solver.
+// smcf = min_cost_flow.SimpleMinCostFlow()
+
+// Define the data
+
+// # Define four parallel arrays: sources, destinations, capacities,
+// # and unit costs between each pair. For instance, the arc from node 0
+// # to node 1 has a capacity of 15.
+// start_nodes = np.array([0, 0, 1, 1, 1, 2, 2, 3, 4])
+// end_nodes = np.array([1, 2, 2, 3, 4, 3, 4, 4, 2])
+// capacities = np.array([15, 8, 20, 4, 10, 15, 4, 20, 5])
+// unit_costs = np.array([4, 4, 2, 2, 6, 1, 3, 2, 3])
+
+// # Define an array of supplies at each node.
+// supplies = [20, 0, 0, -5, -15]
+
+// Add the arcs
+
+// For each start node and end node, we create an arc from start node to end node with the given capacity and unit cost, using the method AddArcWithCapacityAndUnitCost.
+
+// The solver's SetNodeSupply method creates a vector of supplies for the nodes.
+
+// # Add arcs, capacities and costs in bulk using numpy.
+// all_arcs = smcf.add_arcs_with_capacity_and_unit_cost(
+//     start_nodes, end_nodes, capacities, unit_costs
+// )
+
+// # Add supply for each nodes.
+// smcf.set_nodes_supplies(np.arange(0, len(supplies)), supplies)
