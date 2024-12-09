2d16ebdf30a4cd95604ca0e1dd452a6f7fc9cbec
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-12-09 10:36:42 -0800

    Remove to constraint ik namespace.

--- modules/constraint_ik/register_types.h
+++ modules/constraint_ik/register_types.h
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  register_types.cpp                                                    */
+/*  register_types.h                                                      */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -28,41 +28,12 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-#include "register_types.h"
+#ifndef MANY_BONE_IK_REGISTER_TYPES_H
+#define MANY_BONE_IK_REGISTER_TYPES_H
 
-#include "src/ik_bone_3d.h"
-#include "src/ik_effector_3d.h"
-#include "src/ik_effector_template_3d.h"
-#include "src/ik_kusudama_3d.h"
-#include "src/many_bone_ik_3d.h"
+#include "modules/register_module_types.h"
 
-#ifdef TOOLS_ENABLED
-#include "editor/many_bone_ik_3d_gizmo_plugin.h"
-#endif
+void initialize_constraint_ik_module(ModuleInitializationLevel p_level);
+void uninitialize_constraint_ik_module(ModuleInitializationLevel p_level);
 
-void initialize_many_bone_ik_module(ModuleInitializationLevel p_level) {
-	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
-	}
-#ifdef TOOLS_ENABLED
-	if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR) {
-		EditorPlugins::add_by_type<EditorPluginManyBoneIK>();
-	}
-#endif
-	if (p_level == MODULE_INITIALIZATION_LEVEL_SERVERS) {
-		GDREGISTER_CLASS(IKEffectorTemplate3D);
-		GDREGISTER_CLASS(ManyBoneIK3D);
-		GDREGISTER_CLASS(IKBone3D);
-		GDREGISTER_CLASS(IKNode3D);
-		GDREGISTER_CLASS(IKEffector3D);
-		GDREGISTER_CLASS(IKBoneSegment3D);
-		GDREGISTER_CLASS(IKKusudama3D);
-		GDREGISTER_CLASS(IKRay3D);
-		GDREGISTER_CLASS(IKLimitCone3D);
-	}
-}
-
-void uninitialize_many_bone_ik_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
-	}
-}
+#endif // MANY_BONE_IK_REGISTER_TYPES_H
