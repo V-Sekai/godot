f23f7f2d7e71c40a8b4a7b1bbe3d4b6de7411aac
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-01-20 21:37:11 -0800

    Add qcpik. Add some documentation.
    Add validation for joint settings and implement rootmost bone check
    
    Fix rootmost bone validation and adjust bone chain transformation logic
    
    Refactor joint processing logic to improve clarity and efficiency
    
    Fix comments for clarity and adjust bone translation logic in CCDIK3D
    
    Refactor CCDIK3D joint processing by removing unused rotation calculations

--- scene/3d/ccd_ik_3d.cpp
+++ scene/3d/ccd_ik_3d.cpp
@@ -30,78 +30,132 @@
 
 #include "ccd_ik_3d.h"
 
+#include "core/math/qcp.h"
+
+// The basic idea is to create a bunch of ordered sets of bones.
+//
+// TODO: Solve one effector to one Godot Engine Node3D solve.
+//
+// Building each set by starting at an effector and traversing all the way down to the skeleton root. (Keep track of each effector you started from)
+//
+// The effector is the end of the chain, and the root is the beginning of the chain. A root is also the bone rootmost to the `Vector<int> bones_to_process = skeleton->get_parentless_bones()`;
+//
+// Then you take the intersections of all of the bonesets, giving you the sets of bonesets which solve for the same effectors.
+// This allows you to initialize moved/target arrays once for each boneset, since by definition all of the bones inside of them are solving for the same effectors.
+//
+// Finally, determine the default traversal order for the bonesets, then flatten those into a bone traversal list.
+//
+// Assume the existence and usage of the disjoint-set data structure. Do not need to implement it here.
+//
+// In computer science, a disjoint-set data structure, also called a union–find data structure or merge–find set, is a data structure that stores a collection of disjoint (non-overlapping) sets.
+
 void CCDIK3D::_process_joints(double p_delta, Skeleton3D *p_skeleton, Vector<ManyBoneIK3DJointSetting *> &p_joints, Vector<Vector3> &p_chain, const Transform3D &p_space, const Vector3 &p_destination, const Vector3 &p_target_vector, int p_max_iterations, real_t p_min_distance) {
-	double min_distance_sq = p_min_distance * p_min_distance;
-	real_t distance_to_target_sq = INFINITY;
+	ERR_FAIL_NULL(p_skeleton);
+	ERR_FAIL_INDEX(p_joints.size() - 1, p_joints.size());
+	ERR_FAIL_INDEX(0, p_joints.size());
+	ERR_FAIL_COND_MSG(!is_rootmost_bone(p_skeleton, p_joints[0]->bone, p_joints[p_joints.size() - 1]->bone), "The first bone must be the rootmost bone.");
 	int iteration_count = 0;
-
-	// Quaternion destination_rotation = p_space.basis.get_rotation_quaternion() * p_skeleton->get_bone_global_pose(p_joints[p_joints.size() - 1]->bone).basis.get_rotation_quaternion();
-
-	while (distance_to_target_sq > min_distance_sq && iteration_count < p_max_iterations) {
-		iteration_count++;
-
-		// Backwards.
-		int end = -1;
-		for (int i = p_joints.size() - 1; i >= 0; i--) {
-			ManyBoneIK3DSolverInfo *solver_info = p_joints[i]->solver_info;
-			if (!solver_info) {
-				continue;
-			}
-			const int HEAD = i;
-			if (end == -1) {
-				end = i + 1;
-			}
-			Vector3 to_vec = p_destination - p_chain[HEAD];
-			Vector3 head_to_end = p_chain[end] - p_chain[HEAD];
-			Quaternion to_rot = Quaternion(head_to_end.normalized(), to_vec.normalized());
-			for (int j = p_chain.size() - 1; j > i; j--) {
-				Vector3 head_to_any_joint = p_chain[j] - p_chain[HEAD];
-				p_chain.write[j] = p_chain[HEAD] + to_rot.xform(head_to_any_joint);
-
-				// For constraint.
-				/*
-				Vector3 current_head_to_tail = p_skeleton->get_bone_global_pose(p_joints[i]->bone).basis.get_rotation_quaternion().xform_inv(to_vec);
-				Quaternion rotation_result = solver_info->current_rot = Quaternion(-solver_info->forward_vector, current_head_to_tail);
-				Transform3D rest = p_skeleton->get_bone_global_pose(p_joints[i]->bone) * Basis(p_joints[i]->constraint_rotation_offset);
-				Quaternion rest_rotation = rest.basis.get_rotation_quaternion();
-				bool rotation_modified = false;
-				TwistSwing ts = decompose_rotation_to_twist_and_swing(rest_rotation, rotation_result);
-				if (p_joints[i]->twist_limitation < Math_PI) {
-					// TODO: coding which limit twist.
-					// ts.twist = XXXXX;
-					rotation_modified = true;
-				}
-				Ref<IKConstraint3D> constraint = p_joints[i]->constraint;
-				if (constraint.is_valid()) {
-					ts.swing = constraint->solve(rest_rotation, ts.swing);
-					rotation_modified = true;
-				}
-				if (rotation_modified) {
-					// TODO: coding which fix any joint by constraintated rotation.
-					p_chain.write[j] = compose_rotation_from_twist_and_swing(rest_rotation, ts).xform(-solver_info->forward_vector);
-				}
-				*/
-			}
+	Vector<SolverInfoData> solver_info_data;
+	solver_info_data.resize(p_joints.size());
+	for (int joints_i = 0; joints_i < p_joints.size(); ++joints_i) {
+		if (p_joints[joints_i] == nullptr) {
+			continue;
 		}
-
-		distance_to_target_sq = p_chain[p_chain.size() - 1].distance_squared_to(p_destination);
-	}
-
-	for (int i = 0; i < p_joints.size(); i++) {
-		ManyBoneIK3DSolverInfo *solver_info = p_joints[i]->solver_info;
+		ManyBoneIK3DSolverInfo *solver_info = p_joints[joints_i]->solver_info;
 		if (!solver_info) {
 			continue;
 		}
-		Vector3 current_head_to_tail = (p_chain[i + 1] - p_chain[i]).normalized();
-		current_head_to_tail = p_skeleton->get_bone_global_pose(p_joints[i]->bone).basis.get_rotation_quaternion().xform_inv(current_head_to_tail);
-		if (solver_info->forward_vector.dot(current_head_to_tail) > 1.0f - CMP_EPSILON) {
+		SolverInfoData &current_solver_data = solver_info_data.write[joints_i];
+		current_solver_data.target_transform.origin = p_chain[joints_i] + p_destination;
+		current_solver_data.moved_transform.origin = p_chain[joints_i];
+	}
+
+	while (iteration_count < p_max_iterations) {
+		iteration_count++;
+		Vector<Transform3D> moveds;
+		Vector<Transform3D> targets;
+		for (int joints_i = p_joints.size() - 1; joints_i >= 0; --joints_i) {
+			moveds.resize(joints_i + 1);
+			targets.resize(joints_i + 1);
+			for (int inner_joints_i = joints_i; inner_joints_i >= 0; --inner_joints_i) {
+				SolverInfoData &inner_solver_info_data = solver_info_data.write[inner_joints_i];
+				targets.write[inner_joints_i] = inner_solver_info_data.target_transform;
+				moveds.write[inner_joints_i] = inner_solver_info_data.moved_transform;
+			}
+			bool translate = joints_i == 0;
+			Transform3D result = _solve(targets, moveds, translate);
+			solver_info_data.write[joints_i].moved_transform.origin = result.origin;
+		}
+	}
+	for (int joints_i = 0; joints_i < p_joints.size(); ++joints_i) {
+		if (!p_joints[joints_i]) {
 			continue;
 		}
-		solver_info->current_rot = Quaternion(solver_info->forward_vector, current_head_to_tail);
-		p_skeleton->set_bone_pose_rotation(p_joints[i]->bone,
-			get_local_pose_rotation(
-				p_skeleton,
-				p_joints[i]->bone,
-				p_skeleton->get_bone_global_pose(p_joints[i]->bone).basis.get_rotation_quaternion() * solver_info->current_rot));
+		ManyBoneIK3DSolverInfo *solver_info = p_joints[joints_i]->solver_info;
+		if (!solver_info) {
+			continue;
+		}
+		p_chain.write[joints_i] = solver_info_data[joints_i].moved_transform.origin;
+		bool translate = joints_i == 0;
+		if (translate) {
+			p_skeleton->set_bone_pose_position(p_joints[joints_i]->bone, solver_info_data[joints_i].moved_transform.origin);
+		}
 	}
+}
+
+Transform3D CCDIK3D::_solve(const Vector<Transform3D> &p_targets, const Vector<Transform3D> &p_moveds, bool p_translate) {
+	ERR_FAIL_COND_V(p_targets.size() != p_moveds.size(), Transform3D());
+
+	Vector<Vector3> moved_positions;
+	Vector<Vector3> target_positions;
+	Vector<double> weights;
+
+	moved_positions.resize(p_moveds.size() * 7);
+	target_positions.resize(p_targets.size() * 7);
+	weights.resize(p_moveds.size() * 7);
+	real_t pin_weight = 1.0;
+	weights.fill(pin_weight);
+
+	for (int i = 0; i < p_targets.size(); ++i) {
+		moved_positions.write[i * 7] = p_moveds[i].origin;
+		int rest_index = 1;
+		for (int axis_i = Vector3::AXIS_X; axis_i <= Vector3::AXIS_Z; ++axis_i) {
+			Vector3 column = p_moveds[i].basis.get_column(axis_i);
+			moved_positions.write[i * 7 + rest_index] = column + p_moveds[i].origin;
+			rest_index++;
+			moved_positions.write[i * 7 + rest_index] = p_moveds[i].origin - column;
+			rest_index++;
+		}
+
+		target_positions.write[i * 7] = p_targets[i].origin;
+		int current_index = 1;
+		for (int axis_j = Vector3::AXIS_X; axis_j <= Vector3::AXIS_Z; ++axis_j) {
+			Vector3 column = p_targets[i].basis.get_column(axis_j);
+			target_positions.write[i * 7 + current_index] = column + p_targets[i].origin;
+			current_index++;
+			target_positions.write[i * 7 + current_index] = p_targets[i].origin - column;
+			current_index++;
+		}
+	}
+	Quaternion rotation;
+	Vector3 translation;
+	QuaternionCharacteristicPolynomial::weighted_superpose(
+			moved_positions, target_positions, weights, p_translate, rotation, translation);
+	return Transform3D(Basis(rotation), translation);
+}
+
+bool CCDIK3D::is_rootmost_bone(Skeleton3D *p_skeleton, BoneId p_root_bone, BoneId p_leaf_bone) const {
+	if (p_root_bone == p_leaf_bone) {
+		return true;
+	}
+
+	BoneId current_bone = p_leaf_bone;
+	while (current_bone != -1) {
+		if (current_bone == p_root_bone) {
+			return true;
+		}
+		current_bone = p_skeleton->get_bone_parent(current_bone);
+	}
+
+	return false;
 }
