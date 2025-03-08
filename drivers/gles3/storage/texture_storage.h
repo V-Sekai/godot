7bcaaca254362d04fb341f5b87029a7842d643e7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-03-07 20:31:36 -0800

    Restore bind_framebuffer_system

--- drivers/gles3/storage/texture_storage.h
+++ drivers/gles3/storage/texture_storage.h
@@ -704,6 +704,10 @@ public:
 	void bind_framebuffer(GLuint framebuffer) {
 		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 	}
+	
+	void bind_framebuffer_system() {
+		glBindFramebuffer(GL_FRAMEBUFFER, GLES3::TextureStorage::system_fbo);
+	}
 
 	String get_framebuffer_error(GLenum p_status);
 };
