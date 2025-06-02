96d618b3ad53c25af6339ba205a19d2e9ad258f0
werner mendizabal <nonameentername@gmail.com>
2025-06-02 09:20:10 -0500

    Changes to get libgodot working on linux

--- servers/display_server.cpp
+++ servers/display_server.cpp
@@ -1343,8 +1343,8 @@ bool DisplayServer::is_rendering_device_supported() {
 
 	Error err;
 
-#ifdef WINDOWS_ENABLED
-	// On some NVIDIA drivers combining OpenGL and RenderingDevice can result in crash, offload the check to the subprocess.
+#if defined(WINDOWS_ENABLED) || defined(LINUXBSD_ENABLED) && !defined(LIBGODOT_ENABLED)
+	// On some drivers combining OpenGL and RenderingDevice can result in crash, offload the check to the subprocess.
 	List<String> arguments;
 	arguments.push_back("--test-rd-support");
 
