3d5ad0f5036ff28d9804a6ca21818946c79c63f4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-09-27 01:51:25 -0700

    Remove parallel commits.

--- modules/parallel_commits/register_types.cpp
+++
@@ -1,9 +0,0 @@
-/* register_types.cpp */
-
-#include "register_types.h"
-
-#include "core/object/class_db.h"
-
-#include "planner_parallel_commits.h"
-#include "planner_consensus_result.h"
-#include "planner_global_state.h"
