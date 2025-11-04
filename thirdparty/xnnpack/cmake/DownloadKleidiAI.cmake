ccd2a5da2c41a17bcb0fbab6ae4c578914fffd5d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-11-04 14:19:55 -0800

    git subrepo clone https://github.com/google/XNNPACK.git thirdparty/xnnpack
    subrepo:
      subdir:   "thirdparty/xnnpack"
      merged:   "43fd7218328"
    upstream:
      origin:   "https://github.com/google/XNNPACK.git"
      branch:   "master"
      commit:   "43fd7218328"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "4f60dd7"

---
+++ thirdparty/xnnpack/cmake/DownloadKleidiAI.cmake
@@ -0,0 +1,31 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# Copyright 2019 Google LLC
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+CMAKE_MINIMUM_REQUIRED(VERSION 3.5 FATAL_ERROR)
+
+PROJECT(kleidiai-download NONE)
+
+# Set file timestamps to the time of extraction.
+IF(POLICY CMP0135)
+  CMAKE_POLICY(SET CMP0135 NEW)
+ENDIF()
+
+# LINT.IfChange
+INCLUDE(ExternalProject)
+ExternalProject_Add(kleidiai
+  URL https://github.com/ARM-software/kleidiai/archive/d7770c89632329a9914ef1a90289917597639cbe.zip
+  URL_HASH SHA256=ecd433a4f7446f7f02a04e458989de8253f19187aa85e3b81b19e0b60f0bf859
+  SOURCE_DIR "${CMAKE_BINARY_DIR}/kleidiai-source"
+  BINARY_DIR "${CMAKE_BINARY_DIR}/kleidiai"
+  CONFIGURE_COMMAND ""
+  PATCH_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+)
+# LINT.ThenChange(../MODULE.bazel:kleidiai)
