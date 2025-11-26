1e311893b2e7b160b59c188c61d9164b70ded107
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-11-26 05:27:23 -0800

    Add JointLimitationKusudama3D implementation with many_bone_ik algorithm
    - Implement kusudama joint limitation with multiple cones support
    - Replace algorithm with many_bone_ik version for improved accuracy
    - Add center ring visualization with fixed radius to prevent shifting
    - Add comprehensive tests for kusudama joint limitations
    - Update IK iteration to support kusudama constraints
    - Add documentation for JointLimitationKusudama3D class

---
+++ scene/resources/3d/joint_limitation_kusudama_3d.h
@@ -0,0 +1,90 @@
+/**************************************************************************/
+/*  joint_limitation_kusudama_3d.h                                        */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include "scene/resources/3d/joint_limitation_3d.h"
+
+class JointLimitationKusudama3D : public JointLimitation3D {
+	GDCLASS(JointLimitationKusudama3D, JointLimitation3D);
+
+	// Cones data: Vector4(x, y, z, radius) where (x,y,z) is normalized control point
+	// Default to one cone matching JointLimitationCone3D: +Y axis, radius_range 0.25 (0.25 * PI radians)
+	Vector<Vector4> cones = { Vector4(0, 1, 0, 0.7853981633974483) };
+
+	// Axial (twist) limits
+	real_t min_axial_angle = 0.0;
+	real_t range_angle = Math::TAU;
+	bool axially_constrained = false;
+	bool orientationally_constrained = true;
+
+protected:
+	static void _bind_methods();
+	bool _set(const StringName &p_name, const Variant &p_value);
+	bool _get(const StringName &p_name, Variant &r_ret) const;
+	void _get_property_list(List<PropertyInfo> *p_list) const;
+
+	virtual Vector3 _solve(const Vector3 &p_direction) const override;
+
+public:
+	void set_cones(const Vector<Vector4> &p_cones);
+	Vector<Vector4> get_cones() const;
+
+	void set_cone_count(int p_count);
+	int get_cone_count() const;
+
+	void set_cone_center(int p_index, const Vector3 &p_center);
+	Vector3 get_cone_center(int p_index) const;
+
+	void set_cone_center_quaternion(int p_index, const Quaternion &p_quaternion);
+	Quaternion get_cone_center_quaternion(int p_index) const;
+
+	void set_cone_radius(int p_index, real_t p_radius);
+	real_t get_cone_radius(int p_index) const;
+
+	void set_orientationally_constrained(bool p_constrained);
+	bool is_orientationally_constrained() const;
+
+	void set_axial_limits(real_t p_min_angle, real_t p_range_angle);
+	void set_min_axial_angle(real_t p_angle);
+	real_t get_min_axial_angle() const;
+	void set_range_angle(real_t p_angle);
+	real_t get_range_angle() const;
+
+	void set_axially_constrained(bool p_constrained);
+	bool is_axially_constrained() const;
+
+	// Override base class method to apply twist constraints using make_space
+	virtual Vector3 solve(const Vector3 &p_local_forward_vector, const Vector3 &p_local_right_vector, const Quaternion &p_rotation_offset, const Vector3 &p_local_current_vector, const Quaternion &p_rotation = Quaternion(), Quaternion *r_constrained_rotation = nullptr) const override;
+
+#ifdef TOOLS_ENABLED
+	virtual void draw_shape(Ref<SurfaceTool> &p_surface_tool, const Transform3D &p_transform, float p_bone_length, const Color &p_color) const override;
+#endif // TOOLS_ENABLED
+};
