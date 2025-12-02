f43ae6dcbf9b5c28af89f2cf81833764a4597c3a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-12-02 01:21:14 -0800

    Fix tangent storage and shader logic in JointLimitationKusudama3D
    - Swap tangent storage: tan2 in quad[1], tan1 in quad[2] to match shader expectations
    - Fix shader to use tangent_2 for c1c2dir < 0.0 and tangent_1 for c1c2dir >= 0.0
    - Initialize last quad's cone2 as Vector4() to prevent ghost cones
    - Simplify set_cone_center function by removing redundant error checks

--- scene/resources/3d/joint_limitation_kusudama_3d.cpp
+++ scene/resources/3d/joint_limitation_kusudama_3d.cpp
@@ -59,20 +59,22 @@ bool is_in_inter_cone_path(in vec3 normal_dir, in vec4 tangent_1, in vec4 cone_1
 	float c1c2dir = dot(normal_dir, c1xc2);
 
 	if (c1c2dir < 0.0) {
-		vec3 c1xt1 = cross(cone_1.xyz, tangent_1.xyz);
-		vec3 t1xc2 = cross(tangent_1.xyz, cone_2.xyz);
-		float c1t1dir = dot(normal_dir, c1xt1);
-		float t1c2dir = dot(normal_dir, t1xc2);
-
-		return (c1t1dir > 0.0 && t1c2dir > 0.0);
-
-	} else {
+		// Use tangent_2 for this side
 		vec3 t2xc1 = cross(tangent_2.xyz, cone_1.xyz);
 		vec3 c2xt2 = cross(cone_2.xyz, tangent_2.xyz);
 		float t2c1dir = dot(normal_dir, t2xc1);
 		float c2t2dir = dot(normal_dir, c2xt2);
 
 		return (c2t2dir > 0.0 && t2c1dir > 0.0);
+
+	} else {
+		// Use tangent_1 for this side
+		vec3 c1xt1 = cross(cone_1.xyz, tangent_1.xyz);
+		vec3 t1xc2 = cross(tangent_1.xyz, cone_2.xyz);
+		float c1t1dir = dot(normal_dir, c1xt1);
+		float t1c2dir = dot(normal_dir, t1xc2);
+
+		return (c1t1dir > 0.0 && t1c2dir > 0.0);
 	}
 }
 
@@ -444,8 +446,9 @@ void JointLimitationKusudama3D::_update_quad_tangents(int p_quad_index) {
 	}
 	
 	// Store tangents in the quad (columns 1 and 2)
-	quad[1] = Vector4(tan1.x, tan1.y, tan1.z, tan_radius);
-	quad[2] = Vector4(tan2.x, tan2.y, tan2.z, tan_radius);
+	// Swap storage to match shader expectations: tan2 in column 1, tan1 in column 2
+	quad[1] = Vector4(tan2.x, tan2.y, tan2.z, tan_radius);
+	quad[2] = Vector4(tan1.x, tan1.y, tan1.z, tan_radius);
 }
 
 Vector3 JointLimitationKusudama3D::_solve(const Vector3 &p_direction) const {
