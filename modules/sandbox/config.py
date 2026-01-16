c59f7c1f65784730765932ff51e53411346e6c77
K. S. Ernest (iFire) Lee <K. S. Ernest (iFire) Lee>
2026-01-15 16:23:12 -0800

    Revert "Enable sandbox module on Android and iOS platforms"
    This reverts commit af3a79c0567c9229a2546625fd1ff214076f2259.

--- modules/sandbox/config.py
+++ modules/sandbox/config.py
@@ -1,4 +1,8 @@
 def can_build(env, platform):
+    if platform == "android":
+        return False
+    if platform == "ios":
+        return False
     print("Building with ", env)
     if env.get("use_asan", False):
         print("Sandbox module cannot be built with ASAN enabled.")
